a87bb3bfbcdb35543ba5e4662936faa2b7a3e5e6
Stas Okhrym <s.okhrym@gmail.com>
2022-11-29 21:05:48 +0200

    Added validation for create borrow serializer

--- borrow/serializers.py
+++ borrow/serializers.py
@@ -1,5 +1,6 @@
 from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from library.serializers import BookSerializer
 from user.serializers import UserSerializer
@@ -20,6 +21,15 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
         read_only_fields = ("expected_return_date",)
         extra_kwargs = {"days_to_return": {"write_only": True}}
 
+    def validate(self, attrs):
+        data = super(BorrowingCreateSerializer, self).validate(attrs=attrs)
+
+        book = data["book"]
+        if book.inventory < 1:
+            raise ValidationError({"inventory": "There must be books on shelf"})
+
+        return data
+
     def create(self, validated_data):
         with transaction.atomic():
             days_to_return = validated_data.pop("days_to_return")
@@ -27,6 +37,8 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
             book = validated_data.pop("book")
             book.inventory -= 1
             book.save()
+            book.refresh_from_db()
+            validated_data["book"] = book
 
             borrowing = Borrowing.objects.create(**validated_data)
             borrowing.expected_return_date = get_expected_return_date(
