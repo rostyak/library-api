61562f74dda07e6e3e58327f90d17ed696733cbe
Stas Okhrym <s.okhrym@gmail.com>
2022-11-29 13:49:56 +0200

    Added model Borrow into borrow service

--- borrow/models.py
+++ borrow/models.py
@@ -1,3 +1,24 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+def get_expected_return_date(expected_days):
+    current_date = datetime.date.today()
+    return current_date + datetime.timedelta(days=expected_days)
+
+
+class Borrow(models.Model):
+    EXPECTED_DAYS_TO_RETURN = 10
+
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(
+        null=True,
+        default=get_expected_return_date(
+            expected_days=EXPECTED_DAYS_TO_RETURN
+        ),
+    )
+    actual_return_date = models.DateField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"Borrowed at: {self.borrow_date}. Expected to return: {self.expected_return_date}"
