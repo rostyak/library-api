b886e0adf4d40181c9b58cde7b126ea82c86325e
Stas Okhrym <s.okhrym@gmail.com>
2022-11-30 18:06:14 +0200

    Prepared tests for apps and refactored custom user serializer

---
+++ library/test/test_library_api.py
@@ -0,0 +1,67 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+
+from rest_framework.test import APIClient
+
+from library.models import Book
+from library.serializers import BookSerializer
+
+BOOK_URL = reverse("library:book-list")
+
+
+def sample_book(**params):
+    defaults = {
+        "title": "Sample movie",
+        "authors": "Sample authors",
+        "cover": "S",
+        "inventory": 1,
+        "daily_fee": "00.11"
+    }
+    defaults.update(params)
+
+    return Book.objects.create(**defaults)
+
+
+class UnauthenticatedBookApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_no_auth_required(self):
+        res = self.client.get(BOOK_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+class AuthenticatedBookApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_books(self):
+        sample_book()
+        sample_book()
+
+        res = self.client.get(BOOK_URL)
+
+        movies = Book.objects.all().order_by("id")
+        serializer = BookSerializer(movies, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_book_forbidden(self):
+        payload = {
+            "title": "Sample movie",
+            "authors": "Sample authors",
+            "cover": "S",
+            "inventory": 1,
+            "daily_fee": "00.11"
+        }
+        res = self.client.post(BOOK_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
--- user/serializers.py
+++ user/serializers.py
@@ -3,11 +3,10 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff")
-        read_only_fields = ("id", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
+        read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
@@ -16,7 +15,6 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
-
         if password:
             user.set_password(password)
             user.save()
