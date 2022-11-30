d5b00ac8aff8b327df2ec383dae5e57dc3666a08
Stas Okhrym <s.okhrym@gmail.com>
2022-11-30 15:20:23 +0200

    Fixed return-borrowing custom action

--- borrow/views.py
+++ borrow/views.py
@@ -1,5 +1,6 @@
 from rest_framework import mixins, viewsets, generics, status
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -57,7 +58,7 @@ class BorrowingViewSet(
         if self.action == "create":
             return BorrowingCreateSerializer
 
-        if self.action == "return-borrowing":
+        if self.action == "return_borrowing":
             return BorrowingReturnSerializer
 
         return BorrowingSerializer
@@ -68,16 +69,23 @@ class BorrowingViewSet(
     @action(
         methods=["POST"],
         detail=True,
-        url_path="return-borrowing",
+        url_path="return_borrowing",
         permission_classes=[IsAuthenticated],
     )
     def return_borrowing(self, request, pk=None):
-        """Endpoint for uploading image to specific movie"""
         borrowing = self.get_object()
-        serializer = BorrowingReturnSerializer(data=request.data)
+
+        if borrowing.actual_return_date:
+            raise ValidationError("You can not return book twice")
+
+        book = borrowing.book
+        serializer = BorrowingReturnSerializer(borrowing, data=request.data)
 
         if serializer.is_valid():
+            book.inventory += 1
             serializer.save()
+            book.save()
+
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
